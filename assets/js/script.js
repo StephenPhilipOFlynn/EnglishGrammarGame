145636c70233399ba1d9978e2512575b0bda4643
Stephen OFlynn <soflynn@tcd.ie>
2023-08-21 14:43:52 +0000

    change to football quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-let correctAns = 0;
+let correctAns = 7;
 
 function onSubmit() {
   let ans = document.getElementById("answer").value;
@@ -13,16 +13,16 @@ function onSubmit() {
 function onNext() {
   const parentElement = document.getElementById("parent");
   let ques = parentElement.firstElementChild;
-  correctAns = 1;
+  correctAns = 10;
   ques.innerHTML =
-    "What single digit appears most frequently between and including the numbers 1 and 1,000? Hint: look for a pattern!";
+    "What shirt did Diego Maradona wear for Argentina!";
   let inputBox = ques.nextElementSibling;
   inputBox.innerHTML = "";
   let submitButton = document.getElementById("btn1");
   let nextButton = submitButton.nextElementSibling;
   nextButton.innerHTML = "End";
   nextButton.onclick = () => {
-    document.body.innerHTML = "This page is under construction!";
+    document.body.innerHTML = "You reached the end of the quiz congratulations";
   }
   let resP = nextButton.nextElementSibling;
   resP.innerHTML = "";

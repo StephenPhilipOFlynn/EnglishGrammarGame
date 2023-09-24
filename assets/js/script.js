a74e46bd9f6a2f53c7245df860f73804df8c6f47
Stephen OFlynn <soflynn@tcd.ie>
2023-09-24 15:31:14 +0000

    add play again button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,315 +2,315 @@
 
 const questions = [
   {
-  question: "The dog ______ sleeping",
+  question: "1. The dog ______ sleeping",
   answers: [
   { text: "am", correct: false},
   { text: "is", correct: true},
 ]
 },
 {
-  question: "What time ______ it?",
+  question: "2. What time ______ it?",
   answers: [
   { text: "are", correct: false},
   { text: "is", correct: true},
 ]
 },
 {
-  question: "I ______ an English student",
+  question: "3. I ______ an English student",
   answers: [
   { text: "are", correct: false},
   { text: "am", correct: true},
 ]
 },
 {
-  question: "Some people ___ Ireland speak a different language called Gaelic.",
+  question: "4. Some people ___ Ireland speak a different language called Gaelic.",
   answers: [
   { text: "on", correct: false},
   { text: "in", correct: true},
 ]
 },
 {
-  question: "I like to go to Paris _______ the summer.",
+  question: "5. I like to go to Paris _______ the summer.",
   answers: [
   { text: "in", correct: true},
   { text: "on", correct: false},
 ]
 },
 {
-  question: "The cat ______ on the fence",
+  question: "6. The cat ______ on the fence",
   answers: [
   { text: "sit", correct: false},
   { text: "sits", correct: true},
 ]
 },
 {
-  question: "She ate _____ apple",
+  question: "7. She ate _____ apple",
   answers: [
   { text: "an", correct: true},
   { text: "a", correct: false},
 ]
 },
 {
-  question: "They _____ French. They're from Belgium",
+  question: "8. They _____ French. They're from Belgium",
   answers: [
   { text: "aren't", correct: true},
   { text: "isn't", correct: false},
 ]
 },
 {
-  question: "We _____ talking for 2 hours.",
+  question: "9. We _____ talking for 2 hours.",
   answers: [
   { text: "was", correct: false},
   { text: "were", correct: true},
 ]
 },
 {
-  question: "I want to make an omelette, _______ eggs do we have?",
+  question: "10. I want to make an omelette, _______ eggs do we have?",
   answers: [
   { text: "how many", correct: true},
   { text: "how much", correct: false},
 ]
 },
 {
-  question: "I get _____ every morning at 8am.",
+  question: "11. I get _____ every morning at 8am.",
   answers: [
   { text: "out", correct: false},
   { text: "up", correct: true},
 ]
 },
 {
-  question: "What book is _______?",
+  question: "12. What book is _______?",
   answers: [
   { text: "those", correct: false},
   { text: "that", correct: true},
 ]
 },
 {
-  question: "The tv show was successful and  _____ them famous.",
+  question: "13. The tv show was successful and  _____ them famous.",
   answers: [
   { text: "make", correct: false},
   { text: "made", correct: true},
 ]
 },
 {
-  question: "They went to the park ____ Sunday.",
+  question: "14. They went to the park ____ Sunday.",
   answers: [
   { text: "at", correct: false},
   { text: "on", correct: true},
 ]
 },
 {
-  question: "Look at ____ horses in the distance.",
+  question: "15. Look at ____ horses in the distance.",
   answers: [
   { text: "these", correct: false},
   { text: "those", correct: true},
 ]
 },
 {
-  question: "Look at ____ trousers on sale.",
+  question: "16. Look at ____ trousers on sale.",
   answers: [
   { text: "this", correct: false},
   { text: "these", correct: true},
 ]
 },
 {
-  question: "______ of these cities will you visit?",
+  question: "17. ______ of these cities will you visit?",
   answers: [
   { text: "Which", correct: true},
   { text: "When", correct: false},
 ]
 },
 {
-  question: "Lets go to _________ concert.",
+  question: "18. Lets go to _________ concert.",
   answers: [
   { text: "their", correct: true},
   { text: "there", correct: false},
 ]
 },
 {
-  question: "She did really good _______.",
+  question: "19. She did really good _______.",
   answers: [
   { text: "well", correct: true},
   { text: "good", correct: false},
 ]
 },
 {
-  question: "How many children _______ got?",
+  question: "20. How many children _______ got?",
   answers: [
   { text: "they have", correct: false},
   { text: "have they", correct: true},
 ]
 },
 {
-  question: "I don't like Colombian coffee, it is _______ strong for me.",
+  question: "21. I don't like Colombian coffee, it is _______ strong for me.",
   answers: [
   { text: "too", correct: true},
   { text: "such", correct: false},
 ]
 },
 {
-  question: "The dog has ______ my homework",
+  question: "22. The dog has ______ my homework",
   answers: [
   { text: "eaten", correct: true},
   { text: "ate", correct: false},
 ]
 },
 {
-  question: "We _____ to Australia when I was five",
+  question: "23. We _____ to Australia when I was five",
   answers: [
   { text: "moves", correct: false},
   { text: "moved", correct: true},
 ]
 },
 {
-  question: "I wanted to go to the cinema but I couldn't. I _____ studying for the test we had on Monday.",
+  question: "24. I wanted to go to the cinema but I couldn't. I _____ studying for the test we had on Monday.",
   answers: [
   { text: "was", correct: true},
   { text: "been", correct: false},
 ]
 },
 {
-  question: "The music video was expensive to make, it ______ twenty million dollars.",
+  question: "25. The music video was expensive to make, it ______ twenty million dollars.",
   answers: [
   { text: "cost", correct: false},
   { text: "costs", correct: true},
 ]
 },
 {
-  question: "No sooner _______ arrived when the Chief Executive began to speak.",
+  question: "26. No sooner _______ arrived when the Chief Executive began to speak.",
   answers: [
   { text: "I had", correct: false},
   { text: "had I", correct: true},
 ]
 },
 {
-  question: "He enjoys ________ the piano in his free time",
+  question: "27. He enjoys ________ the piano in his free time",
   answers: [
   { text: "to play", correct: false},
   { text: "playing", correct: true},
 ]
 },
 {
-  question: "Have you ________ watching without me?",
+  question: "28. Have you ________ watching without me?",
   answers: [
   { text: "begun", correct: true},
   { text: "began", correct: false},
 ]
 },
 {
-  question: "I had _____ all night to see her.",
+  question: "29. I had _____ all night to see her.",
   answers: [
   { text: "drove", correct: false},
   { text: "driven", correct: true},
 ]
 },
 {
-  question: "She works as a teacher in ___ Africa.",
+  question: "30. She works as a teacher in ___ Africa.",
   answers: [
   { text: "__", correct: true},
   { text: "an", correct: false},
 ]
 },
 {
-  question: "I went to ___ bed early yesterday.",
+  question: "31. I went to ___ bed early yesterday.",
   answers: [
   { text: "the", correct: false},
   { text: "_", correct: true},
 ]
 },
 {
-  question: "My sister Ruth is ________ so much progress with her Spanish.",
+  question: "32. My sister Ruth is ________ so much progress with her Spanish.",
   answers: [
   { text: "making", correct: true},
   { text: "taking", correct: false},
 ]
 },
 {
-  question: "I am  ________ learn to dance.",
+  question: "33. I am  ________ learn to dance.",
   answers: [
   { text: "going", correct: false},
   { text: "going to", correct: true},
 ]
 },
 {
-  question: "What  ________ happen if she doesn't get here on time?",
+  question: "34. What  ________ happen if she doesn't get here on time?",
   answers: [
   { text: "will", correct: true},
   { text: "going to", correct: false},
 ]
 },
 {
-  question: "He ______________ there for two years before he decided to resign.",
+  question: "35. He ______________ there for two years before he decided to resign.",
   answers: [
   { text: "had been worked", correct: false},
   { text: "had been working", correct: true},
 ]
 },
 {
-  question: "At one time it ___________ the earth was the center of the universe.",
+  question: "36. At one time it ___________ the earth was the center of the universe.",
   answers: [
   { text: "was being believed", correct: false},
   { text: "was believed", correct: true},
 ]
 },
 {
-  question: "When she was a child, she ______ be happy all the time.",
+  question: "37. When she was a child, she ______ be happy all the time.",
   answers: [
   { text: "would", correct: false},
   { text: "used to", correct: true},
 ]
 },
 {
-  question: "______ you like a tea?",
+  question: "38. ______ you like some tea?",
   answers: [
   { text: "Will", correct: false},
   { text: "Would", correct: true},
 ]
 },
 {
-  question: "_____ Indian food?",
+  question: "39. _____ Indian food?",
   answers: [
   { text: "Did she ever ate", correct: false},
   { text: "Has she ever eaten", correct: true},
 ]
 },
 {
-  question: "They have not finished interviews yet, but they ____________ all candidates by next Friday",
+  question: "40. They have not finished interviews yet, but they ____________ all candidates by next Friday",
   answers: [
   { text: "would see", correct: false},
   { text: "will have seen", correct: true},
 ]
 },
 {
-  question: "You ___________ bothered making pasta, they've decided to head out.",
+  question: "41. You ___________ bothered making pasta, they've decided to head out.",
   answers: [
   { text: "needn't have", correct: true},
   { text: "didn't have to", correct: false},
 ]
 },
 {
-  question: "Stephen _______ watching chess matches.",
+  question: "42. Stephen _______ watching chess matches.",
   answers: [
   { text: "'s always loved", correct: true},
   { text: "'s always been loving", correct: false},
 ]
 },
 {
-  question: "The meeting finished late. Isabel told me she _____ been given the wrong directions.",
+  question: "43. The meeting finished late. Isabel told me she _____ been given the wrong directions.",
   answers: [
   { text: "had", correct: true},
   { text: "has", correct: false},
 ]
 },
 {
-  question: "Da Vinci's lost painting! To think it was lost for years and _________.",
+  question: "44. Da Vinci's lost painting! To think it was lost for years and _________.",
   answers: [
   { text: "has been restoring", correct: false},
   { text: "had to be restored", correct: true},
 ]
 },
 {
-  question: "I am not surprised your stomach is sore. I did say you __________ eaten that fish last night.",
+  question: "45. I am not surprised your stomach is sore. I did say you __________ eaten that fish last night.",
   answers: [
   { text: "mustn't have", correct: false},
   { text: "shouldn't have", correct: true},
@@ -327,6 +327,7 @@ const ready = document.getElementById("ready");
 const questionText = document.getElementById("questionArea");
 const buttons = document.querySelectorAll(".button");
 const result = document.getElementById("result");
+const startAgain = document.getElementById("startAgain");
 
 function startQuiz(){
   introText.style.display = "none";
@@ -368,6 +369,7 @@ function displayNextQuestion() {
 
 function displayResult() {
   result.textContent = "Congrats you have finished the quiz!"
+  startAgain.style.display = "block";
   // add play again function option here //
 }
 
